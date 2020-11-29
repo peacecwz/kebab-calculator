2be8fa84181fc127a9eef3fe80d81db84be63518
PeaceCwz (Baris Ceviz) <me@barisceviz.com>
2020-11-30 02:41:47 +0300

    Implement Kebap Service

--- src/MLSA.KebabCalculator.Functions/Services/KebabServices.cs
+++ src/MLSA.KebabCalculator.Functions/Services/KebabServices.cs
@@ -1,83 +1,95 @@
 using System.Collections.Generic;
 using System.Linq;
+using MLSA.KebabCalculator.Functions.HealthRules;
+using MLSA.KebabCalculator.Functions.HealthRules.Rules;
 using MLSA.KebabCalculator.Functions.Models;
 
 namespace MLSA.KebabCalculator.Functions.Services
 {
     public class KebabServices : IKebabServices
     {
-        private readonly List<KebabMaterial> _kebapMaterials;
+        private readonly List<KebabMaterial> _kebabMaterials;
 
         public KebabServices()
         {
-            _kebapMaterials = new List<KebabMaterial>()
-            {
-                new KebabMaterial()
-                {
-                    Name = "Dana Kıyma",
-                    Amount = 350,
-                    AmountType = AmountType.Gram
-                },
-                new KebabMaterial()
-                {
-                    Name = "Kuzu Kıyma",
-                    Amount = 300,
-                    AmountType = AmountType.Gram
-                },
-                new KebabMaterial()
-                {
-                    Name = "Diş sarmısak",
-                    Amount = 1,
-                    AmountType = AmountType.Piece
-                },
-                new KebabMaterial()
-                {
-                    Name = "Kuru Soğan",
-                    Amount = 2,
-                    AmountType = AmountType.Piece
-                },
-                new KebabMaterial()
-                {
-                    Name = "Pul biber",
-                    Amount = 5,
-                    AmountType = AmountType.Gram
-                },
-                new KebabMaterial()
-                {
-                    Name = "Karabiber",
-                    Amount = 5,
-                    AmountType = AmountType.Gram
-                },
-                new KebabMaterial()
-                {
-                    Name = "Acı salça",
-                    Amount = 25,
-                    AmountType = AmountType.Gram
-                },
-                new KebabMaterial()
-                {
-                    Name = "Maydonoz",
-                    Amount = 0.5,
-                    AmountType = AmountType.Piece
-                },
-                new KebabMaterial()
-                {
-                    Name = "Sıvı Yağ",
-                    Amount = 60,
-                    AmountType = AmountType.Piece
-                },
-            };
+            _kebabMaterials = GetKebabMaterials();
         }
 
         public List<KebabMaterial> GetFoodMaterials(int peopleCount)
         {
-            return _kebapMaterials.Select(material => new KebabMaterial()
+            return _kebabMaterials.Select(material => new KebabMaterial()
             {
                 AmountType = material.AmountType,
                 Name = material.Name,
                 Amount = material.Amount * peopleCount,
                 Notice = ""
             }).ToList();
+        }
+
+        private static List<KebabMaterial> GetKebabMaterials()
+        {
+            return new List<KebabMaterial>()
+            {
+                new KebabMaterial
+                {
+                    Name = GroundLambHealthRule.MaterialName,
+                    Amount = 1000,
+                    AmountType = AmountType.Gram
+                },
+                new KebabMaterial
+                {
+                    Name = GroundBeefHealthRule.MaterialName,
+                    Amount = 1000,
+                    AmountType = AmountType.Gram
+                },
+                new KebabMaterial
+                {
+                    Name = OnionHealthRule.MaterialName,
+                    Amount = 1,
+                    AmountType = AmountType.Piece
+                },
+                new KebabMaterial
+                {
+                    Name = GarlicHealthRule.MaterialName,
+                    Amount = 4,
+                    AmountType = AmountType.Piece
+                },
+                new KebabMaterial
+                {
+                    Name = CuminHealthRule.MaterialName,
+                    Amount = 1.5,
+                    AmountType = AmountType.Piece,
+                    PieceType = "teaspoons"
+                },
+                new KebabMaterial
+                {
+                    Name = SumacHealthRule.MaterialName,
+                    Amount = 1.5,
+                    AmountType = AmountType.Piece,
+                    PieceType = "teaspoons"
+                },
+                new KebabMaterial
+                {
+                    Name = SaltHealthRule.MaterialName,
+                    Amount = 0.5,
+                    AmountType = AmountType.Piece,
+                    PieceType = "teaspoons"
+                },
+                new KebabMaterial
+                {
+                    Name = BlackPaperHealthRule.MaterialName,
+                    Amount = 0.25,
+                    AmountType = AmountType.Piece,
+                    PieceType = "teaspoons"
+                },
+                new KebabMaterial
+                {
+                    Name = RedPaperHealthRule.MaterialName,
+                    Amount = 0.25,
+                    AmountType = AmountType.Piece,
+                    PieceType = "teaspoons"
+                },
+            };
         }
     }
 
